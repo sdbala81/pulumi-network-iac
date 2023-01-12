36b662961f224474dce8cd7603d3030440939d80
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2023-01-12 14:44:22 +0530

    updated git

--- Program.cs
+++ Program.cs
@@ -1,110 +1,117 @@
 ﻿using System.Collections.Generic;
-using Pulumi.AzureNative.ContainerInstance;
-using Pulumi.AzureNative.ContainerInstance.Inputs;
-using Pulumi.AzureNative.Network;
-using Pulumi.AzureNative.Resources;
-using Deployment = Pulumi.Deployment;
+using Pulumi;
+using Pulumi.Azure.ContainerService;
+using Pulumi.Azure.ContainerService.Inputs;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Network;
+using Pulumi.Azure.Network.Inputs;
 
 return await Deployment.RunAsync(() =>
 {
-    // Create an Azure Resource Group
-    var resourceGroup = new ResourceGroup("bala-container-test-group");
-
-    string clientName = "dhl-international";
-    var containerGroupName = $"{clientName}-ci";
-    
-    // Create a container group.
-    var containerGroup = new ContainerGroup("my-container-group", new ContainerGroupArgs
+    // Create a resource group.
+    var resourceGroup = new ResourceGroup("lipton-brokebonds", new ResourceGroupArgs
     {
-        ImageRegistryCredentials = new ImageRegistryCredentialArgs
-        {
-            Server = "docker.cloudsmith.io",
-            Username = "element-data/own_pacages",
-            Password = "bY3ZyXppYmzUQ3Hp"
-        },
+        Name = "lipton-brokebonds",
+        Location = "westeurope"
+    });
+    // Create a virtual network.
+    var network = new VirtualNetwork("lipton-brokebonds-network", new VirtualNetworkArgs
+    {
         ResourceGroupName = resourceGroup.Name,
-        ContainerGroupName = containerGroupName,
+        Location = resourceGroup.Location,
+        AddressSpaces = { "10.0.0.0/16" }
+    });
+    // Create a subnet
+    var subnet = new Subnet("lipton-brokebonds-subnet", new SubnetArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        VirtualNetworkName = network.Name,
+        AddressPrefixes = new[]
+        {
+            "10.0.2.0/24"
+        }
+    });
+    // Create a container group.
+    var containerGroup = new Group("lipton-brokebonds-container-group", new GroupArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
         OsType = "Linux",
+        IpAddressType = "Private",
+        SubnetIds = subnet.Id,
         Containers = new[]
         {
-            new ContainerArgs
+            new GroupContainerArgs
             {
-                Image =
-                    "docker.cloudsmith.io/element-data/own_pacages/sampledockerwebapplication:latest",
-                Name = $"{clientName}containers",
+                Name = "lipton-brokebonds-container",
+                Image = "docker.cloudsmith.io/element-data/own_pacages/sampledockerwebapplication:latest",
+                Cpu = 0.5,
+                Memory = 0.5,
                 Ports = new[]
                 {
-                    new ContainerPortArgs { Port = 80 }
-                },
-                Resources = new ResourceRequirementsArgs
-                {
-                    Requests = new ResourceRequestsArgs
+                    new GroupContainerPortArgs
                     {
-                        Cpu = 1.0,
-                        MemoryInGB = 1.5
+                        Port = 80
                     }
                 }
             }
         },
-        IpAddress = new IpAddressArgs
+        ImageRegistryCredentials = new[]
         {
-            Ports = new[]
+            new GroupImageRegistryCredentialArgs
             {
-                new PortArgs
-                {
-                    Port = 80,
-                    Protocol = "TCP"
-                }
-            },
-            Type = "Public"
-        },
-        RestartPolicy = "always",
+                Server = "docker.cloudsmith.io",
+                Username = "element-data/own_pacages",
+                Password = "bY3ZyXppYmzUQ3Hp"
+            }
+        }
     });
-
-    // Create a network interface.
-    var networkInterface = new NetworkInterface("my-network-interface", new NetworkInterfaceArgs
+    // Create a network security group and a rule to allow traffic from a specific IP.
+    var networkSecurityGroup = new NetworkSecurityGroup("lipton-brokebonds-nsg", new NetworkSecurityGroupArgs
     {
         ResourceGroupName = resourceGroup.Name,
-        IpConfigurations =
-        {
-            new NetworkInterfaceIpConfigurationArgs
-            {
-                Name = "my-ip-config",
-                SubnetId = subnet.Id,
-                PrivateIpAddressAllocation = "Dynamic",
-            },
-        },
-        ContainerGroup = containerGroup,
-    });
-
-    // Create a network security group.
-    var networkSecurityGroup = new NetworkSecurityGroup("my-nsg", new NetworkSecurityGroupArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
         SecurityRules =
         {
             new NetworkSecurityGroupSecurityRuleArgs
             {
                 Name = "allow-http",
                 Protocol = "Tcp",
-                SourceAddressPrefix = "1.2.3.4/32",
+                SourceAddressPrefix = "123.231.110.255/32",
+                DestinationAddressPrefix = "*",
                 DestinationPortRange = "80",
                 Access = "Allow",
                 Priority = 100,
                 Direction = "Inbound",
-            },
-        },
+                SourcePortRange = "80"
+            }
+        }
+    });
+    // Create a network interface.
+    var networkInterface = new NetworkInterface("lipton-brokebonds-network-interface", new NetworkInterfaceArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
+        IpConfigurations = new[]
+        {
+            new NetworkInterfaceIpConfigurationArgs
+            {
+                Name = "internal",
+                SubnetId = subnet.Id,
+                PrivateIpAddressAllocation = "Dynamic"
+            }
+        }
     });
     // Add the network interface to the security group.
-    var networkSecurityGroupAssociation = new NetworkInterfaceSecurityGroupAssociation("my-nsg-assoc", new NetworkInterfaceSecurityGroupAssociationArgs
-    {
-        NetworkInterfaceId = networkInterface.Id,
-        NetworkSecurityGroupId = networkSecurityGroup.Id,
-    });
-    
+    var networkSecurityGroupAssociation = new NetworkInterfaceSecurityGroupAssociation("lipton-brokebonds-nsg-assoc",
+        new NetworkInterfaceSecurityGroupAssociationArgs
+        {
+            NetworkInterfaceId = networkInterface.Id,
+            NetworkSecurityGroupId = networkSecurityGroup.Id
+        });
     // Export the primary key of the Storage Account
     return new Dictionary<string, object?>
     {
-        ["primaryStorageKey"] = primaryStorageKey
+        ["container_ip"] = containerGroup.IpAddress
     };
 });
